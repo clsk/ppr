a46286834b266bb1e4540797bc3722752e2089cb
Gogims <gogims@gmail.com>
2014-11-23 17:37:44 -0400

    Salvando user schedule en BD
    Agregue controller, ajax script y la ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 match '/importer/index', :to => 'importer#index', :via => [:get, :post]
 match '/importer/match', :to => 'importer#match', :via => [:get, :post]
 match '/importer/result', :to => 'importer#result', :via => [:get, :post]
+
+resources :user_schedule_entry do
+    member do
+        get 'create2'
+    end
+end
