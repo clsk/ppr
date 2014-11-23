9c8a9761fd67f8a54e3cadebd930a0e67af7ceef
Héctor D. Vásquez Román <hvasquezroman@gmail.com>
2014-11-23 14:46:15 -0400

    Funcionamiento del front-end completo Queda pendiente el evento que utilize el controlador al moemento de guardar la forma y los arreglos de la vista.

--- app/models/user_schedule_entry.rb
+++ app/models/user_schedule_entry.rb
@@ -1,16 +1,3 @@
 class UserScheduleEntry < ActiveRecord::Base
-  unloadable
-
-    def to_s
-        str= ""
-        str_days= self.days_of_week.to_s(2)
-        days= ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
-
-        for i in 1 .. days.length
-            str+= day[i] + "," if str_days[i] == 1
-        end
-
-        str+= " - " + self.hours
-        str
-    end
+    unloadable
 end
