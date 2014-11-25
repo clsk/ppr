14f1cd88327ecf00bac60c1f773bcca66d12e0c0
Héctor D. Vásquez Román <hvasquezroman@gmail.com>
2014-11-25 14:50:24 -0400

    Cambie el textbox de las horas por un combobox

--- app/controllers/user_schedule_entry_controller.rb
+++ app/controllers/user_schedule_entry_controller.rb
@@ -1,18 +1,20 @@
 class UserScheduleEntryController < ApplicationController
+
+    def create2
+        parsed_json= ActiveSupport::JSON.decode(params[:entries])
+
+        UserScheduleEntry.where(:user_id => params[:id]).delete_all
     
-  def create2
-		parsed_json = ActiveSupport::JSON.decode(params[:entries])
-		
-		UserScheduleEntry.where(:user_id => params[:id]).delete_all
-				
-		parsed_json.each do |d, h|
-		  user_schedule = UserScheduleEntry.new()
-		  user_schedule.days_of_week = d
-		  user_schedule.hours = h
-		  user_schedule.user_id = params[:id]		  
-		  user_schedule.save
-		end
-		
-		render :json => 	{ :size => 1 }
-	end
+        parsed_json.each do |d, h|
+            user_schedule= UserScheduleEntry.new()
+
+            user_schedule.hours= h
+            user_schedule.days_of_week= d
+            user_schedule.user_id= params[:id]		  
+
+            user_schedule.save
+        end
+
+        render :json=> { :size => 1 }
+    end
 end
