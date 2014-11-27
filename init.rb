aec8169172acfec1e9519bb2e662eb3ef17b5381
Héctor D. Vásquez Román <hvasquezroman@gmail.com>
2014-11-27 10:15:56 -0400

    Actualización de la ruta del view

--- init.rb
+++ init.rb
@@ -3,15 +3,16 @@
 require 'redmine'
 
 Redmine::Plugin.register :ppr do
-  name 'Issue Importer'
-  author 'Martin Liu / Leo Hourvitz / Stoyan Zhekov / Jérôme Bataille'
-  description 'Issue import plugin for Redmine.'
-  version '1.2.2'
+    name 'Issue Importer'
+    author 'Martin Liu / Leo Hourvitz / Stoyan Zhekov / Jérôme Bataille'
+    description 'Issue import plugin for Redmine.'
+    version '1.2.2'
 
-  project_module :importer do
-    permission :import, :importer => :index
-  end
-  menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
+    project_module :importer do
+        permission :import, :importer => :index
+    end
+
+    menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
 end
 
 require 'ppr/hooks/user_schedule_entries_hook'
--- lib/ppr/hooks/user_schedule_entries_hook.rb
+++ lib/ppr/hooks/user_schedule_entries_hook.rb
@@ -1,15 +1,7 @@
 module PPR
-	module Hooks
-	  class UserScheduleEntriesHook < Redmine::Hook::ViewListener
-		# This just renders the partial in
-		# app/views/hooks/my_plugin/_view_issues_form_details_bottom.rhtml
-		# The contents of the context hash is made available as local variables to the partial.
-		#
-		# Additional context fields
-		#   :issue  => the issue this is edited
-		#   :f      => the form object to create additional fields
-		render_on :view_users_form,
-				  :partial => 'ppr/user_schedule_entries'
-	end
-  end
+    module Hooks
+        class UserScheduleEntriesHook < Redmine::Hook::ViewListener
+            render_on :view_users_form, :partial => 'ppr/user/user_schedule_entries'
+        end
+    end
 end
