435414446a93289c41f83d70a6e1472b4904eebb
Héctor D. Vásquez Román <hvasquezroman@gmail.com>
2014-11-30 02:38:27 -0400

    Funcionalidad #4, 100%. Pendiente de testeo

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -15,11 +15,17 @@ class ResourcesController < ApplicationController
 
         @calendar = Redmine::Helpers::Calendar.new(Date.civil(@year, @month, 1), current_language, :month)
 
+        @resources= {}
+        User.select("id, firstname, lastname").where("type = 'User'").each do |u|
+            @resources[u.id]= { :name=> ("#{u.firstname} #{u.lastname}"), :selected=> false }
+        end
+
         events= []
-        @resources= []
         if params[:selected_columns]
             params[:selected_columns].each do |r|
-                @resources << r.to_i
+                @resources[r.to_i][:selected]= true
+                @resources[r.to_i][:colour]= randhexlightcolour
+
                 Issue.select("start_date, due_date, sum(estimated_hours) as estimated_hours").where("assigned_to_id = ? and ((start_date between ? and ?) or (due_date between ? and ?))", r.to_i, @calendar.startdt, @calendar.enddt, @calendar.startdt, @calendar.enddt).group("start_date, due_date").each do |i|
                     events << PPR::Events::ResourceScheduleEvent.new(i.start_date, i.due_date, r.to_i, i.estimated_hours)
                 end
@@ -27,6 +33,33 @@ class ResourcesController < ApplicationController
         end
 
         @calendar.events= events
-        @users= User.select("id, firstname, lastname").order("firstname, lastname")
     end
+
+    private
+        # Amount should be a decimal between 0 and 1. Lower means darker
+        def darken_colour(hex_color, amount=0.4)
+            hex_color= hex_color.gsub('#','')
+            rgb= hex_color.scan(/../).map { |color| color.hex }
+
+            rgb[0]= (rgb[0].to_i * amount).round
+            rgb[1]= (rgb[1].to_i * amount).round
+            rgb[2]= (rgb[2].to_i * amount).round
+
+            "%02x%02x%02x" % rgb
+        end
+
+        # Amount should be a decimal between 0 and 1. Higher means lighter
+        def lighten_colour(hex_color, amount=0.6)
+            hex_color= hex_color.gsub('#','')
+            rgb= hex_color.scan(/../).map { |color| color.hex }
+
+            rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+            rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+            rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+
+            "%02x%02x%02x" % rgb
+        end
+        def randhexlightcolour
+            "##{ lighten_colour(darken_colour(("%06x" % (rand * 0xffffff)).to_s, 0.4), 0.4) }"
+        end
 end
