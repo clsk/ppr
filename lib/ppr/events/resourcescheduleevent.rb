f9a3492199607b74d0008842c380c72d84c8b076
Héctor D. Vásquez Román <hvasquezroman@gmail.com>
2014-12-02 12:25:29 -0400

    Eliminando archivos basura

--- lib/ppr/events/resourcescheduleevent.rb
+++ lib/ppr/events/resourcescheduleevent.rb
@@ -1,13 +1,13 @@
 module PPR
     module Events
-        class ResourceScheduleEvent < BaseEvent
-            attr_reader :id, :hours
+        class ResourceScheduleEvent
+            attr_reader :id, :hours, :start_date, :due_date
 
-            def initialize(start_date, due_date, id, hours)
-                super(start_date, due_date)
-
+            def initialize(id, hours, start_date, due_date)
                 @id= id
                 @hours= hours
+                @due_date= due_date
+                @start_date= start_date
             end
         end
     end
